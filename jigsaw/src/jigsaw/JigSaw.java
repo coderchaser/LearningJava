c590d8c064cb0b88a1dda23beed7778549029f0a
bobliao <codechaser@163.com>
2017-11-28 09:44:56 +0800

    Upload JigSaw.java

--- jigsaw/src/jigsaw/JigSaw.java
+++ jigsaw/src/jigsaw/JigSaw.java
@@ -4,20 +4,23 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.util.Collections;
+import java.util.Iterator;
 
 
 
 public class JigSaw {
 	static final int RC=4;
 	final int N=RC*RC;
-	//private int[] blockList=new int[N];
+	private int[] blockList={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 	private JFrame frame=new JFrame();
 	private JPanel panel1=new JPanel();
 	private JPanel panel2=new JPanel();
@@ -35,11 +38,26 @@ public class JigSaw {
 		frame.setVisible(true);
 		this.startButton.addActionListener((e) -> {
 			startButtonClicked();
+			this.startButton.setEnabled(false);
 		});
 	}
 	private void startButtonClicked() {
 		//打乱块之间的顺序
-		
+		ArrayList<Integer> blockList1=new ArrayList<Integer>(); 
+		for (int i:this.blockList){
+			blockList1.add(i);
+		}
+		Collections.shuffle(blockList1);
+		Iterator ite=blockList1.iterator();
+		for(int i=0;i<N;i++){
+			this.buttons[i].setText(""+ite.next());
+			if(!ite.hasNext()) break;
+		}
+		int i=findblank(blockList1);
+		this.buttons[i].setVisible(false);
+	}
+	private int findblank(ArrayList<Integer> blockList1) {
+		return blockList1.indexOf(N);
 	}
 	private void setInitState() {
 		this.panel1.setLayout(new GridLayout(RC, RC, 5, 5));
